bdc2b0b7ba42061ff9f52509558d64eefdbb92c3
Joseph Vint <josephvint@me.com>
2018-02-11 20:45:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,3 @@
 def reverse_each_word(string)
-  array = string.split(" ")
-  array.collect do |index|
-    index.reverse
-  end.join(" ")
+  string.split.collect {|word| word.reverse}.join(" ")
 end
